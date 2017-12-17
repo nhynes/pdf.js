215a0d1815733ad649c735bdd5378176983658e9
Nick Hynes <nhynes@mit.edu>
2017-12-16 21:41:15 -0800

    Make max auto zoom smaller

--- web/ui_utils.js
+++ web/ui_utils.js
@@ -21,7 +21,7 @@ const DEFAULT_SCALE = 1.0;
 const MIN_SCALE = 0.10;
 const MAX_SCALE = 10.0;
 const UNKNOWN_SCALE = 0;
-const MAX_AUTO_SCALE = 1.25;
+const MAX_AUTO_SCALE = 0.90;
 const SCROLLBAR_PADDING = 40;
 const VERTICAL_PADDING = 5;
 
